223ff0042d845f1c69bb2368fca8547896cd8dce
jwdeitch <jwdeitch@gmail.com>
2018-07-24 22:26:02 -0400

    progress

--- src/plot.rs
+++ src/plot.rs
@@ -5,15 +5,19 @@ pub fn plot(x: f64, y: f64, z: f64)
     let mut fg = Figure::new();
     fg.set_terminal("png", "fig.png");
 
+    let mut xs: Vec<f64> = vec![0, x];
+    let mut ys: Vec<f64> = vec![0, y];
+    let mut zs: Vec<f64> = vec![0, z];
+
     fg.axes3d()
         .set_title("3D lines + points", &[])
-        .lines(vec![0, 1], vec![0, 1], vec![0, 1],
+        .lines(vec![0, xs], vec![0, ys], vec![0, zs],
                &[LineWidth(5.0),
                    PointSymbol('o'),
                    Color("#ffaa77"),
                    PointSize(2.0),
                    ArrowType(Filled),
-                   ArrowSize(10.2)]);
+                   ArrowSize(1.2)]);
 
 
     fg.show();
