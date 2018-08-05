2b5d7425f2cea81f7d3d184ccc1f911285eada77
jwdeitch <jwdeitch@gmail.com>
2018-08-05 16:03:09 -0400

    start plotting trait

--- src/plot.rs
+++ src/plot.rs
@@ -4,6 +4,18 @@ use serde_json::{Value, Error};
 use spin::SpinConfiguration;
 use serde_json;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct CartesianPoint {
+    x: f64,
+    y: f64,
+    z: Option<f64>
+}
+
+trait Plot {
+    fn get_points() -> Vec<CartesianPoint>;
+}
+
+
 pub fn write_plot(spin_configs: Vec<SpinConfiguration>) {
     let mut file = File::create("figs/data.json").unwrap();
     write!(file, "{}", serde_json::to_string(&spin_configs).unwrap());
