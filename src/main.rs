b6d88e3e6176dd7bf5df1b9eb1f7335e14a00ba8
Patrick O'Brien <patrick@commonbond.co>
2018-08-06 18:33:08 -0400

    Add magnetization.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 extern crate num;
-extern crate num;
-extern crate num;
 
 mod spin;
 mod exchange_matrix;
@@ -30,15 +28,6 @@ use num::abs;
 
 fn main()
 {
-//    let simulation =  MonteCarlo::metrolpolis_algorithm(20, 20);
-//    println!("{:?}", simulation);
-
-    let mut ising_spin_config = run_ising_monte_carlo(4, 4, 1000, 200, 3.0);
-
-    ising_spin_config.print_spin_configuration();
-
-    println!("The magnetization is: {}",magnetization(&ising_spin_config));
-
-    println!("{}", abs(-1.0))
-
+    let simulation =  MonteCarlo::metrolpolis_algorithm(20, 20);
+    println!("{:?}", simulation);
 }
